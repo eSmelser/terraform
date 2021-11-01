af0451c59ca17aa844c87b7f09dd1d6b23bba852
Evan Smelser <evansmelser@evans-mbp.lan>
2021-11-01 15:39:09 -0700

    rename main tf file and adds default vpc specification

---
+++ prod.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "prod_tf_course" {
+  bucket = "tf-course-20211101"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "default" {}
+
