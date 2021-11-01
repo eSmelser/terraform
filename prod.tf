51fdb5a0db878b51b6f687b9bbcb7117c74e91cd
Evan Smelser <evansmelser@evans-mbp.lan>
2021-11-01 15:52:44 -0700

    define initial web security group

--- prod.tf
+++ prod.tf
@@ -10,3 +10,31 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "allow standard http and https ports inbound and everything outbound"
+  
+  ingress {
+    from_port  = 80
+    to_port    = 80
+    protocol   = "tcp"
+    cidr_blocks = ["76.27.205.71/32"]
+  }
+  ingress {
+    from_port  = 443 
+    to_port    = 443
+    protocol   = "tcp"
+    cidr_blocks = ["76.27.205.71/32"]
+  }
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+
+}
