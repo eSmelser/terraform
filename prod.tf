a91e2c4026987ec414028716b921d13900740f1d
Evan Smelser <evansmelser@evans-mbp.lan>
2021-11-03 15:20:04 -0700

    replaces raw instances with auto scaling group

--- prod.tf
+++ prod.tf
@@ -6,9 +6,27 @@ provider "aws" {
 resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-course-20211101"
   acl    = "private"
+
+  tags = {
+    "Terraform" : "true"
+  }
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
@@ -36,5 +54,51 @@ resource "aws_security_group" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
 
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-091dc5ac6b1d84e27"
+  instance_type = "t2.nano"
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+#  availability_zones  = ["us-west-2a","us-west-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  desired_capacity    = 1 
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Terraform" 
+    value               =  "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  tags = {
+    "Terraform" : "true"
+  }
 }
